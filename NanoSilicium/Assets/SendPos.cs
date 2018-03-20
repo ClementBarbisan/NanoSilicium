ba1f0039fbdd066c788072ff556dffeb47866b1c
Clément Barbisan <c.barbisan@free.Fr>
2018-03-20 08:09:36 +0100

    add built-in shaders

---
+++ NanoSilicium/Assets/SendPos.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SendPos : MonoBehaviour {
+    public Material shader;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!shader)
+            return;
+        float[] floatArray = { gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z, 1.0f };
+        shader.SetFloatArray("_position", floatArray);
+	}
+}
