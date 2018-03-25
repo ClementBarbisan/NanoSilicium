32985cadc0a0e7a83bb301989e509b52184ea8db
Clément Barbisan <c.barbisan@free.Fr>
2018-03-25 15:37:20 +0200

    update

---
+++ NanoSilicium/Assets/Script/IntestinsManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntestinsManager : MonoBehaviour {
+    public static IntestinsManager Instance;
+    public List<GameObject> intestins;
+    public GameObject prefab;
+
+    private void Awake()
+    {
+        Instance = this;
+        intestins = new List<GameObject>();
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp(KeyCode.Space))
+        {
+            GameObject go = Instantiate<GameObject>(prefab, new Vector3(0, 0, 0), Quaternion.Euler(90, 0, 0));
+            intestins.Add(go);
+        }
+	}
+}
